f2b19eb50de0efbb1a3b3227bfebfff83c67388b
Adebayo Oluwadunsin Iyanuoluwa <oiadebayo11@student.lautech.edu.ng>
2023-12-27 19:56:19 +0100

    Updated main.py and Readme.md
    Updated main.py to use correct input structure
    Updated Readme.md to be more descriptive

--- Readme.md
+++ Readme.md
@@ -3,28 +3,28 @@
 This repository contains the solution to the Dwarf-Giant game exercise for the SURIDATA task.
 
 
-## Exercise Overview
+## Purpose of the Project
 
-The task is to implement a "Dwarf-Giant" game using Python. The game involves creating unique pairs of employees, ensuring each employee acts as both a dwarf and a giant exactly once.
+The project is to implement a "Dwarf-Giant" game using Python. The game involves creating unique pairs of employees, ensuring each employee acts as both a dwarf and a giant exactly once.
 
-### Exercise Guidelines
+### Key Features
 
 1. **Input Validation:**
    - Handle duplicates in the input.
-   - The unique index is a combination of the employee's name, role, and ID.
+   - The unique index is a combination of the employee's name, department, and age.
 
 2. **Random Output:**
-   - The output should be random on each execution.
+   - The output will be random on each execution.
 
 3. **Pair Combination:**
-   - Forbid pairs such as (employee_1, employee_2) and (employee_2, employee_1).
+   - It forbid pairs such as (employee_1, employee_2) and (employee_2, employee_1).
 
 4. **Output Format:**
-   - The output must be a list of tuples, where each tuple consists of the dwarf's name and the giant's name.
+   - The output is a list of tuples, where each tuple consists of the dwarf's name and the giant's name.
 
-### Bonus Task
+### Bonus Implementation
 
-Implement a solution that processes the input in multiple chunks using multiple processes.
+- Solution process multiple chunks with multiple threads.
 
 ## Code Overview
 
@@ -52,4 +52,38 @@ The Python script provided in this repository demonstrates the implementation of
 The script generates output in the form of a list of dwarf-giant pairs. Example:
 
 ```python
-[('John', 'Alice'), ('Bob', 'John'), ('Abiodun', 'Jane'), ('Iyanu', 'Abiodun'), ('Alice', 'Bob'), ('Jane', 'Iyanu')]
+[('Reed Parks', 'Taniyah Ramos'), ('Louis Mcintosh', 'Quinn Reese'), ('Kristin Dorsey', 'Terrance Gallagher'),
+('Nadia Hernandez', 'Jayleen Henry'), ('Evelyn Roth', 'Jay Gonzales'), ('Finley Vang', 'Brayden Young'),
+('Jaydin Braun', 'Gerald Booker'), ('Alvaro Haley', 'Kaleb Benitez'), ('Maria Kelley', 'Shayna Burke'),
+('Grayson Barrera', 'Joyce Randolph'), ('Heaven Bartlett', 'Carolina Rowe'), ('Zack Barnes', 'Darnell Rangel'),
+('Taniyah Ramos', 'Saniyah Luna'), ('Jorge Good', 'Heaven Bartlett'), ('Axel Bolton', 'Abril Schaefer'),
+('Saniyah Luna', 'Jason Jimenez'), ('Madilyn Melton', 'Alan Kemp'), ('Alan Kemp', 'Oliver Mcconnell'),
+('Brayden Young', 'Omar Browning'), ('Dalia Gomez', 'Eli Buck'), ('Dario Robertson', 'Madilyn Melton'),
+('Jaden Hardin', 'Jaidyn Noble'), ('Joyce Randolph', 'Bennett Wolf'), ('Jaidyn Noble', 'Desiree Carey'),
+('Gerald Booker', 'Jorge Good'), ('Kristopher Sanders', 'Francisco Escobar'), ('Shyann Harmon', 'Saige Castro'),
+('Eli Buck', 'Elliot Cantu'), ('Jadon Tucker', 'Charlize Cobb'), ('Fiona Pearson', 'Houston Nguyen'),
+('Lillie Pollard', 'Devin Benitez'), ('Nikolas Porter', 'Esmeralda Harris'), ('Yuram Henson', 'Riley Fowler'),
+('Leila Becker', 'Fiona Pearson'), ('Erica Bullock', 'Reed Parks'), ('Jazmine Massey', 'Nikolas Porter'),
+('Rhianna Potter', 'Erica Bullock'), ('Harry Peterson', 'Aimee Mcpherson'), ('Shayna Burke', 'Abby Zuniga'),
+('Koen Luna', 'Camron Jacobs'), ('Abigayle Sosa', 'Hayden Massey'), ('Grady Baird', 'Elisha Andrade'),
+('Bennett Wolf', 'Kendall Cochran'), ('Elisha Andrade', 'Maria Kelley'), ('Kenyon York', 'Lilliana Wood'),
+('Nadia David', 'Messiah Glover'), ('Mina Caldwell', 'Jazmine Massey'), ('Jayleen Henry', 'Alvaro Haley'),
+('Royce Moore', 'Koen Luna'), ('Saige Castro', 'Sterling Walton'), ('Sasha Horn', 'Rhianna Potter'),
+('Amiah Powell', 'Kristopher Sanders'), ('Hayden Massey', 'Brendon Osborne'), ('Devin Benitez', 'Zack Barnes'),
+('Anthony Bray', 'Shea Robles'), ('Kelvin Cameron', 'Lindsey Hines'), ('Charlize Cobb', 'Ellis Davenport'),
+('Riley Fowler', 'Jaydin Braun'), ('Carolina Rowe', 'Jadon Tucker'), ('Elliot Cantu', 'Abigayle Sosa'),
+('June Hanna', 'Grady Baird'), ('Jordyn May', 'Ralph Yu'), ('Kaleb Benitez', 'Zackary Nguyen'),
+('Zackary Nguyen', 'Shyann Harmon'), ('Melody Nielsen', 'Katrina Hanna'), ('Wilson Medina', 'Kenyon York'),
+('Vaughn Phelps', 'Jimena Roman'), ('Ellis Davenport', 'Dario Robertson'), ('Ralph Yu', 'Nadia Hernandez'),
+('Desiree Carey', 'Kristin Dorsey'), ('Messiah Glover', 'Simon Walker'), ('Jay Gonzales', 'Axel Bolton'),
+('Abby Zuniga', 'Anthony Bray'), ('Abril Schaefer', 'Melody Nielsen'), ('Kylan Cantrell', 'Mariyah Park'),
+('Sterling Walton', 'Donovan Petersen'), ('Simon Walker', 'Amiah Powell'), ('Tate Yates', 'Jaden Hardin'),
+('Aracely Nguyen', 'Sasha Horn'), ('Brendon Osborne', 'Harry Peterson'), ('Kaiya Fry', 'Yuram Henson'),
+('Esmeralda Harris', 'Nadia David'), ('Oliver Mcconnell', 'Kelvin Cameron'), ('Mariyah Park', 'Aracely Nguyen'),
+('Lilliana Wood', 'Evelyn Roth'), ('Darnell Rangel', 'Vaughn Phelps'), ('Aimee Mcpherson', 'Aditya Wilkerson'),
+('Donovan Petersen', 'Lillie Pollard'), ('Francisco Escobar', 'Grayson Barrera'), ('Camron Jacobs', 'Yuram Henson'),
+('Shea Robles', 'Tate Yates'), ('Lindsey Hines', 'Ariel Reed'), ('Jason Jimenez', 'Kenyon Patel'),
+('Terrance Gallagher', 'Kaiya Fry'), ('Houston Nguyen', 'Jordyn May'), ('Aditya Wilkerson', 'Clara Bradley'),
+('Clara Bradley', 'Finley Vang'), ('Omar Browning', 'June Hanna'), ('Quinn Reese', 'Wilson Medina'),
+('Katrina Hanna', 'Louis Mcintosh'), ('Kendall Cochran', 'Royce Moore'), ('Justice Boyle', 'Dalia Gomez'),
+('Kenyon Patel', 'Leila Becker'), ('Ariel Reed', 'Oliver Mcconnell'), ('Jimena Roman', 'Kylan Cantrell')]
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def validate_and_clean(employee_template):
     seen_combinations = set()
 
     for employee in employee_template:
-        combination = (employee["name"], employee["role"], employee["id"])
+        combination = (employee["name"], employee["department"], employee["age"])
         if combination not in seen_combinations:
             seen_combinations.add(combination)
             unique_employees.append(employee)
@@ -17,7 +17,7 @@ def validate_and_clean(employee_template):
     return unique_employees
 
 
-# The function below pairs the employee following the guidlines provided
+# The function below pairs the employee following the guidelines provided
 def create_pairs(employee_list, queue, paired_set, lock):
     random.shuffle(employee_list)
     pairs = set()
@@ -53,25 +53,554 @@ def process_chunk(employee_template, queue, paired_set, lock):
 
 # Main execution block with sample data
 def main():
-    employee_template = [
-        {"name": "John", "role": "CEO", "id": 1},
-        {"name": "Jane", "role": "CTO", "id": 2},
-        {"name": "Bob", "role": "Manager", "id": 3},
-        {"name": "Alice", "role": "Backend Dev", "id": 4},
-        {"name": "Abiodun", "role": "Backend Dev", "id": 5},
-        {"name": "Iyanu", "role": "Backend Dev", "id": 6},
+    employees = [
+        {
+            "department": "R&D",
+            "name": "Nikolas Porter",
+            "age": 46
+        },
+        {
+            "department": "Sales",
+            "name": "Sterling Walton",
+            "age": 28
+        },
+        {
+            "department": "R&D",
+            "name": "Louis Mcintosh",
+            "age": 33
+        },
+        {
+            "department": "R&D",
+            "name": "Joyce Randolph",
+            "age": 29
+        },
+        {
+            "department": "R&D",
+            "name": "Oliver Mcconnell",
+            "age": 63
+        },
+        {
+            "department": "Support",
+            "name": "Jimena Roman",
+            "age": 22
+        },
+        {
+            "department": "Support",
+            "name": "Ellis Davenport",
+            "age": 25
+        },
+        {
+            "department": "Sales",
+            "name": "Jorge Good",
+            "age": 29
+        },
+        {
+            "department": "R&D",
+            "name": "Sasha Horn",
+            "age": 50
+        },
+        {
+            "department": "Sales",
+            "name": "Aracely Nguyen",
+            "age": 63
+        },
+        {
+            "department": "Support",
+            "name": "Kenyon York",
+            "age": 71
+        },
+        {
+            "department": "R&D",
+            "name": "Oliver Mcconnell",
+            "age": 63
+        },
+        {
+            "department": "Sales",
+            "name": "Saniyah Luna",
+            "age": 27
+        },
+        {
+            "department": "R&D",
+            "name": "Abigayle Sosa",
+            "age": 29
+        },
+        {
+            "department": "R&D",
+            "name": "Elisha Andrade",
+            "age": 24
+        },
+        {
+            "department": "Sales",
+            "name": "Abril Schaefer",
+            "age": 22
+        },
+        {
+            "department": "R&D",
+            "name": "Katrina Hanna",
+            "age": 21
+        },
+        {
+            "department": "R&D",
+            "name": "Kaiya Fry",
+            "age": 28
+        },
+        {
+            "department": "R&D",
+            "name": "Shyann Harmon",
+            "age": 33
+        },
+        {
+            "department": "R&D",
+            "name": "Darnell Rangel",
+            "age": 31
+        },
+        {
+            "department": "R&D",
+            "name": "Kendall Cochran",
+            "age": 37
+        },
+        {
+            "department": "R&D",
+            "name": "Kylan Cantrell",
+            "age": 28
+        },
+        {
+            "department": "R&D",
+            "name": "Amiah Powell",
+            "age": 29
+        },
+        {
+            "department": "R&D",
+            "name": "Maria Kelley",
+            "age": 22
+        },
+        {
+            "department": "R&D",
+            "name": "Jay Gonzales",
+            "age": 43
+        },
+        {
+            "department": "R&D",
+            "name": "Shea Robles",
+            "age": 29
+        },
+        {
+            "department": "R&D",
+            "name": "Kenyon Patel",
+            "age": 23
+        },
+        {
+            "department": "Support",
+            "name": "Esmeralda Harris",
+            "age": 33
+        },
+        {
+            "department": "Sales",
+            "name": "Donovan Petersen",
+            "age": 23
+        },
+        {
+            "department": "Sales",
+            "name": "Ralph Yu",
+            "age": 25
+        },
+        {
+            "department": "R&D",
+            "name": "Nadia Hernandez",
+            "age": 26
+        },
+        {
+            "department": "Support",
+            "name": "Finley Vang",
+            "age": 27
+        },
+        {
+            "department": "R&D",
+            "name": "Kelvin Cameron",
+            "age": 44
+        },
+        {
+            "department": "Support",
+            "name": "Zack Barnes",
+            "age": 23
+        },
+        {
+            "department": "R&D",
+            "name": "Evelyn Roth",
+            "age": 23
+        },
+        {
+            "department": "Sales",
+            "name": "Charlize Cobb",
+            "age": 47
+        },
+        {
+            "department": "Sales",
+            "name": "Devin Benitez",
+            "age": 23
+        },
+        {
+            "department": "R&D",
+            "name": "Jaidyn Noble",
+            "age": 70
+        },
+        {
+            "department": "Sales",
+            "name": "Jaydin Braun",
+            "age": 63
+        },
+        {
+            "department": "Sales",
+            "name": "Anthony Bray",
+            "age": 51
+        },
+        {
+            "department": "Sales",
+            "name": "Leila Becker",
+            "age": 55
+        },
+        {
+            "department": "R&D",
+            "name": "Simon Walker",
+            "age": 57
+        },
+        {
+            "department": "R&D",
+            "name": "Alan Kemp",
+            "age": 22
+        },
+        {
+            "department": "R&D",
+            "name": "Elliot Cantu",
+            "age": 23
+        },
+        {
+            "department": "Support",
+            "name": "Fiona Pearson",
+            "age": 25
+        },
+        {
+            "department": "Sales",
+            "name": "Clara Bradley",
+            "age": 23
+        },
+        {
+            "department": "R&D",
+            "name": "Aimee Mcpherson",
+            "age": 38
+        },
+        {
+            "department": "R&D",
+            "name": "Quinn Reese",
+            "age": 43
+        },
+        {
+            "department": "Sales",
+            "name": "Houston Nguyen",
+            "age": 33
+        },
+        {
+            "department": "Support",
+            "name": "Jayleen Henry",
+            "age": 37
+        },
+        {
+            "department": "Sales",
+            "name": "Terrance Gallagher",
+            "age": 23
+        },
+        {
+            "department": "R&D",
+            "name": "Axel Bolton",
+            "age": 23
+        },
+        {
+            "department": "Sales",
+            "name": "Dario Robertson",
+            "age": 23
+        },
+        {
+            "department": "Sales",
+            "name": "Nadia David",
+            "age": 82
+        },
+        {
+            "department": "Support",
+            "name": "Grayson Barrera",
+            "age": 30
+        },
+        {
+            "department": "Support",
+            "name": "Lillie Pollard",
+            "age": 30
+        },
+        {
+            "department": "R&D",
+            "name": "Desiree Carey",
+            "age": 30
+        },
+        {
+            "department": "R&D",
+            "name": "Jaden Hardin",
+            "age": 30
+        },
+        {
+            "department": "Sales",
+            "name": "Jason Jimenez",
+            "age": 30
+        },
+        {
+            "department": "Sales",
+            "name": "Jordyn May",
+            "age": 32
+        },
+        {
+            "department": "Sales",
+            "name": "Alvaro Haley",
+            "age": 33
+        },
+        {
+            "department": "Sales",
+            "name": "Zackary Nguyen",
+            "age": 54
+        },
+        {
+            "department": "Support",
+            "name": "Lilliana Wood",
+            "age": 42
+        },
+        {
+            "department": "Support",
+            "name": "Koen Luna",
+            "age": 24
+        },
+        {
+            "department": "Sales",
+            "name": "Taniyah Ramos",
+            "age": 21
+        },
+        {
+            "department": "Sales",
+            "name": "Messiah Glover",
+            "age": 20
+        },
+        {
+            "department": "R&D",
+            "name": "Jazmine Massey",
+            "age": 29
+        },
+        {
+            "department": "Support",
+            "name": "Carolina Rowe",
+            "age": 28
+        },
+        {
+            "department": "Sales",
+            "name": "Heaven Bartlett",
+            "age": 26
+        },
+        {
+            "department": "Sales",
+            "name": "Harry Peterson",
+            "age": 44
+        },
+        {
+            "department": "Sales",
+            "name": "Francisco Escobar",
+            "age": 69
+        },
+        {
+            "department": "Support",
+            "name": "Brendon Osborne",
+            "age": 65
+        },
+        {
+            "department": "Support",
+            "name": "Yuram Henson",
+            "age": 55
+        },
+        {
+            "department": "R&D",
+            "name": "Lindsey Hines",
+            "age": 48
+        },
+        {
+            "department": "R&D",
+            "name": "Brayden Young",
+            "age": 35
+        },
+        {
+            "department": "Support",
+            "name": "Rhianna Potter",
+            "age": 30
+        },
+        {
+            "department": "Support",
+            "name": "June Hanna",
+            "age": 19
+        },
+        {
+            "department": "R&D",
+            "name": "Eli Buck",
+            "age": 20
+        },
+        {
+            "department": "R&D",
+            "name": "Shayna Burke",
+            "age": 65
+        },
+        {
+            "department": "Support",
+            "name": "Kristopher Sanders",
+            "age": 62
+        },
+        {
+            "department": "R&D",
+            "name": "Tate Yates",
+            "age": 52
+        },
+        {
+            "department": "Sales",
+            "name": "Hayden Massey",
+            "age": 50
+        },
+        {
+            "department": "R&D",
+            "name": "Grady Baird",
+            "age": 51
+        },
+        {
+            "department": "Sales",
+            "name": "Dalia Gomez",
+            "age": 44
+        },
+        {
+            "department": "Support",
+            "name": "Riley Fowler",
+            "age": 48
+        },
+        {
+            "department": "R&D",
+            "name": "Madilyn Melton",
+            "age": 37
+        },
+        {
+            "department": "R&D",
+            "name": "Omar Browning",
+            "age": 39
+        },
+        {
+            "department": "R&D",
+            "name": "Melody Nielsen",
+            "age": 44
+        },
+        {
+            "department": "R&D",
+            "name": "Camron Jacobs",
+            "age": 30
+        },
+        {
+            "department": "R&D",
+            "name": "Royce Moore",
+            "age": 41
+        },
+        {
+            "department": "Support",
+            "name": "Ariel Reed",
+            "age": 42
+        },
+        {
+            "department": "Support",
+            "name": "Yuram Henson",
+            "age": 55
+        },
+        {
+            "department": "Sales",
+            "name": "Kaleb Benitez",
+            "age": 51
+        },
+        {
+            "department": "Sales",
+            "name": "Mariyah Park",
+            "age": 52
+        },
+        {
+            "department": "Sales",
+            "name": "Saige Castro",
+            "age": 40
+        },
+        {
+            "department": "Sales",
+            "name": "Kristin Dorsey",
+            "age": 61
+        },
+        {
+            "department": "Support",
+            "name": "Reed Parks",
+            "age": 62
+        },
+        {
+            "department": "R&D",
+            "name": "Gerald Booker",
+            "age": 43
+        },
+        {
+            "department": "Support",
+            "name": "Bennett Wolf",
+            "age": 23
+        },
+        {
+            "department": "Support",
+            "name": "Abby Zuniga",
+            "age": 30
+        },
+        {
+            "department": "R&D",
+            "name": "Vaughn Phelps",
+            "age": 19
+        },
+        {
+            "department": "R&D",
+            "name": "Aditya Wilkerson",
+            "age": 27
+        },
+        {
+            "department": "Support",
+            "name": "Jadon Tucker",
+            "age": 26
+        },
+        {
+            "department": "Sales",
+            "name": "Erica Bullock",
+            "age": 34
+        },
+        {
+            "department": "Sales",
+            "name": "Wilson Medina",
+            "age": 36
+        },
+        {
+            "department": "Support",
+            "name": "Justice Boyle",
+            "age": 37
+        },
+        {
+            "department": "Support",
+            "name": "Mina Caldwell",
+            "age": 44
+        }
     ]
 
     with Manager() as manager:
         employee_queue = manager.Queue()
-        for employee in employee_template:
+        for employee in employees:
             employee_queue.put(employee)
 
         paired_set = set()
         lock = manager.Lock()
 
         with Pool() as pool:
-            results = pool.starmap(process_chunk, [(employee_template, employee_queue, paired_set, lock)])
+            results = pool.starmap(process_chunk, [(employees, employee_queue, paired_set, lock)])
 
     final_output = [pair for sublist in results for pair in sublist]
     print(final_output)
@@ -80,5 +609,45 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+
+
+
+
+
 #  Example Output:
-# [('John', 'Alice'), ('Bob', 'John'), ('Abiodun', 'Jane'), ('Iyanu', 'Abiodun'), ('Alice', 'Bob'), ('Jane', 'Iyanu')]
+# [('Reed Parks', 'Taniyah Ramos'), ('Louis Mcintosh', 'Quinn Reese'), ('Kristin Dorsey', 'Terrance Gallagher'),
+# ('Nadia Hernandez', 'Jayleen Henry'), ('Evelyn Roth', 'Jay Gonzales'), ('Finley Vang', 'Brayden Young'),
+# ('Jaydin Braun', 'Gerald Booker'), ('Alvaro Haley', 'Kaleb Benitez'), ('Maria Kelley', 'Shayna Burke'),
+# ('Grayson Barrera', 'Joyce Randolph'), ('Heaven Bartlett', 'Carolina Rowe'), ('Zack Barnes', 'Darnell Rangel'),
+# ('Taniyah Ramos', 'Saniyah Luna'), ('Jorge Good', 'Heaven Bartlett'), ('Axel Bolton', 'Abril Schaefer'),
+# ('Saniyah Luna', 'Jason Jimenez'), ('Madilyn Melton', 'Alan Kemp'), ('Alan Kemp', 'Oliver Mcconnell'),
+# ('Brayden Young', 'Omar Browning'), ('Dalia Gomez', 'Eli Buck'), ('Dario Robertson', 'Madilyn Melton'),
+# ('Jaden Hardin', 'Jaidyn Noble'), ('Joyce Randolph', 'Bennett Wolf'), ('Jaidyn Noble', 'Desiree Carey'),
+# ('Gerald Booker', 'Jorge Good'), ('Kristopher Sanders', 'Francisco Escobar'), ('Shyann Harmon', 'Saige Castro'),
+# ('Eli Buck', 'Elliot Cantu'), ('Jadon Tucker', 'Charlize Cobb'), ('Fiona Pearson', 'Houston Nguyen'),
+# ('Lillie Pollard', 'Devin Benitez'), ('Nikolas Porter', 'Esmeralda Harris'), ('Yuram Henson', 'Riley Fowler'),
+# ('Leila Becker', 'Fiona Pearson'), ('Erica Bullock', 'Reed Parks'), ('Jazmine Massey', 'Nikolas Porter'),
+# ('Rhianna Potter', 'Erica Bullock'), ('Harry Peterson', 'Aimee Mcpherson'), ('Shayna Burke', 'Abby Zuniga'),
+# ('Koen Luna', 'Camron Jacobs'), ('Abigayle Sosa', 'Hayden Massey'), ('Grady Baird', 'Elisha Andrade'),
+# ('Bennett Wolf', 'Kendall Cochran'), ('Elisha Andrade', 'Maria Kelley'), ('Kenyon York', 'Lilliana Wood'),
+# ('Nadia David', 'Messiah Glover'), ('Mina Caldwell', 'Jazmine Massey'), ('Jayleen Henry', 'Alvaro Haley'),
+# ('Royce Moore', 'Koen Luna'), ('Saige Castro', 'Sterling Walton'), ('Sasha Horn', 'Rhianna Potter'),
+# ('Amiah Powell', 'Kristopher Sanders'), ('Hayden Massey', 'Brendon Osborne'), ('Devin Benitez', 'Zack Barnes'),
+# ('Anthony Bray', 'Shea Robles'), ('Kelvin Cameron', 'Lindsey Hines'), ('Charlize Cobb', 'Ellis Davenport'),
+# ('Riley Fowler', 'Jaydin Braun'), ('Carolina Rowe', 'Jadon Tucker'), ('Elliot Cantu', 'Abigayle Sosa'),
+# ('June Hanna', 'Grady Baird'), ('Jordyn May', 'Ralph Yu'), ('Kaleb Benitez', 'Zackary Nguyen'),
+# ('Zackary Nguyen', 'Shyann Harmon'), ('Melody Nielsen', 'Katrina Hanna'), ('Wilson Medina', 'Kenyon York'),
+# ('Vaughn Phelps', 'Jimena Roman'), ('Ellis Davenport', 'Dario Robertson'), ('Ralph Yu', 'Nadia Hernandez'),
+# ('Desiree Carey', 'Kristin Dorsey'), ('Messiah Glover', 'Simon Walker'), ('Jay Gonzales', 'Axel Bolton'),
+# ('Abby Zuniga', 'Anthony Bray'), ('Abril Schaefer', 'Melody Nielsen'), ('Kylan Cantrell', 'Mariyah Park'),
+# ('Sterling Walton', 'Donovan Petersen'), ('Simon Walker', 'Amiah Powell'), ('Tate Yates', 'Jaden Hardin'),
+# ('Aracely Nguyen', 'Sasha Horn'), ('Brendon Osborne', 'Harry Peterson'), ('Kaiya Fry', 'Yuram Henson'),
+# ('Esmeralda Harris', 'Nadia David'), ('Oliver Mcconnell', 'Kelvin Cameron'), ('Mariyah Park', 'Aracely Nguyen'),
+# ('Lilliana Wood', 'Evelyn Roth'), ('Darnell Rangel', 'Vaughn Phelps'), ('Aimee Mcpherson', 'Aditya Wilkerson'),
+# ('Donovan Petersen', 'Lillie Pollard'), ('Francisco Escobar', 'Grayson Barrera'), ('Camron Jacobs', 'Yuram Henson'),
+# ('Shea Robles', 'Tate Yates'), ('Lindsey Hines', 'Ariel Reed'), ('Jason Jimenez', 'Kenyon Patel'),
+# ('Terrance Gallagher', 'Kaiya Fry'), ('Houston Nguyen', 'Jordyn May'), ('Aditya Wilkerson', 'Clara Bradley'),
+# ('Clara Bradley', 'Finley Vang'), ('Omar Browning', 'June Hanna'), ('Quinn Reese', 'Wilson Medina'),
+# ('Katrina Hanna', 'Louis Mcintosh'), ('Kendall Cochran', 'Royce Moore'), ('Justice Boyle', 'Dalia Gomez'),
+# ('Kenyon Patel', 'Leila Becker'), ('Ariel Reed', 'Oliver Mcconnell'), ('Jimena Roman', 'Kylan Cantrell')]
